462acc78337a096d4c0315b380be04fca8cef308
jaimereycasado <jaimereycasado@gmail.com>
2014-12-09 03:04:03 +0100

    Third commit cap.45

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,26 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('ListaPelisCtrl', ['$scope', 'rtmFactory',
+      function($scope, rtmFactory) {
+        $scope.countries = rtmFactory.getCountries();
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+        $scope.loadMovies = function( countyCode ) {
 
-  }]);
+          rtmFactory.getMovies( countyCode).then(
+
+              function( response ) {
+                var movieArray = response.data.movies;
+                console.log(JSON.stringify( movieArray ));
+                $scope.movies = movieArray;
+              },
+
+              function( response ){
+                //error message
+              }
+          )
+        };
+
+        $scope.loadMovies('us');
+      }
+    ]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,31 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+
+    .value('version', '0.1')
+
+    .value('API_KEY', '4djr2qbbezzubhrzmx6au2jq')
+
+    .factory('rtmFactory', ['$http', 'API_KEY', function( $http, API_KEY ){
+
+      var countries = [
+        {name: 'USA', code: 'us'},
+        {name: 'Reino Unido', code: 'uk'},
+        {name: 'Francia', code: 'fr'}
+      ];
+
+      return {
+
+        getCountries: function() {
+          return countries;
+        },
+
+        getMovies: function( countryCode ){
+
+          return $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=10&country='+countryCode+'&callback=JSON_CALLBACK&apikey='+API_KEY)
+
+        }
+      }
+
+    }]);
